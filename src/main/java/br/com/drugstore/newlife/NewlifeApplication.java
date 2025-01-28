4f52713142efeac055d09acc9347317be8324267
deisesalless <deisesalles18@gmail.com>
2025-01-27 22:34:21 -0300

    1.0 upload project

---
+++ src/main/java/br/com/drugstore/newlife/NewlifeApplication.java
@@ -0,0 +1,13 @@
+package br.com.drugstore.newlife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewlifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewlifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/drugstore/newlife/NewlifeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.drugstore.newlife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewlifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
