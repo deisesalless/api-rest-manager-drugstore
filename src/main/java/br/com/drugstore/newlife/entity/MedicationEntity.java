db3c648d207e158d9a6ec45ccdd11a3c1518553d
deisesalless <deisesalles18@gmail.com>
2025-01-31 23:50:35 -0300

    3.4 criação da tabela medication com o flyway e finalização da classe medication entity

--- src/main/java/br/com/drugstore/newlife/entity/MedicationEntity.java
+++ src/main/java/br/com/drugstore/newlife/entity/MedicationEntity.java
@@ -2,7 +2,7 @@ package br.com.drugstore.newlife.entity;
 
 import br.com.drugstore.newlife.enums.Laboratory;
 import br.com.drugstore.newlife.enums.PharmaceuticalForm;
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -10,12 +10,21 @@ import java.util.UUID;
 
 @Data
 @Entity
+@Table(name = "tb_medication")
 public class MedicationEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String name;
     private String concentration;
+
+    @Convert(converter = PharmaceuticalForm.class)
     private PharmaceuticalForm pharmaceuticalForm;
+
+    @Convert(converter = Laboratory.class)
     private Laboratory loboratory;
+
     private LocalDate expirationDate;
     private Double salePrice;
 }
---
+++ src/main/resources/db.migrations/V1__create_table_medication.sql
@@ -0,0 +1,9 @@
+CREATE TABLE tb_medication (
+    id BINARY(16) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    concentration VARCHAR(255),
+    pharmaceuticalForm TINYINT NOT NULL,
+    laboratory TINYINT NOT NULL,
+    expirationDate DATE,
+    salePrice DOUBLE
+);
