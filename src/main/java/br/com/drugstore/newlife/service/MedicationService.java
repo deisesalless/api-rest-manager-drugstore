2001c979bcf5b73956dcbfe2dbccb51644f32307
deisesalless <deisesalles18@gmail.com>
2025-02-02 19:41:07 -0300

    2.0 incluido anotação @Transactional no método saveMedication para garantiar a execução, finalizado o método find all medication para listar todas as medications, padronizando assim o tipo de resposta nos casos de listagens que será diferente do request e response dos casos save medication

--- src/main/java/br/com/drugstore/newlife/service/MedicationService.java
+++ src/main/java/br/com/drugstore/newlife/service/MedicationService.java
@@ -1,15 +1,18 @@
 package br.com.drugstore.newlife.service;
 
 import br.com.drugstore.newlife.dto.MedicationCreateDTO;
+import br.com.drugstore.newlife.dto.MedicationCreatedDTO;
 import br.com.drugstore.newlife.dto.MedicationDTO;
 import br.com.drugstore.newlife.entity.MedicationEntity;
 import br.com.drugstore.newlife.repository.MedicationRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class MedicationService {
@@ -20,14 +23,17 @@ public class MedicationService {
     @Autowired
     private ObjectMapper objectMapper;
 
-    public MedicationDTO saveMedication(MedicationCreateDTO dto) {
+    @Transactional
+    public MedicationCreatedDTO saveMedication(MedicationCreateDTO dto) {
         MedicationEntity medicationEntity = repository.save(objectMapper.convertValue(dto, MedicationEntity.class));
-        return objectMapper.convertValue(medicationEntity, MedicationDTO.class);
+        return objectMapper.convertValue(medicationEntity, MedicationCreatedDTO.class);
     }
 
+
     public List<MedicationDTO> findAllMedication() {
-        List<MedicationEntity> entityList = repository.findAll();
-        return null;
+        return repository.findAll().stream()
+                .map(medication -> objectMapper.convertValue(
+                        medication, MedicationDTO.class)).collect(Collectors.toList());
     }
 
     public MedicationDTO updateMedication(MedicationDTO dto, UUID id) {
