50371f2c8485434f6f8757a10993f84a3976802a
deisesalless <deisesalles18@gmail.com>
2025-02-02 19:38:19 -0300

    1.0 criado um record medication created DTO para padronizar o response nos casos de create para uma nova medicação

---
+++ src/main/java/br/com/drugstore/newlife/dto/MedicationCreatedDTO.java
@@ -0,0 +1,20 @@
+package br.com.drugstore.newlife.dto;
+
+import br.com.drugstore.newlife.enums.Laboratory;
+import br.com.drugstore.newlife.enums.PharmaceuticalForm;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public record MedicationCreatedDTO(
+        UUID id,
+        String name,
+        String concentration,
+        PharmaceuticalForm pharmaceuticalForm,
+        Laboratory laboratory,
+        LocalDate registrationDate,
+        LocalDate expirationDate,
+        Integer quantityInStock,
+        Double costPrice,
+        Double salePrice) {
+}
